e408e80cac2b06cfc298e2c835fd5a0a19a4a4a6
Lyumih <32732179+Lyumih@users.noreply.github.com>
2019-09-07 21:05:08 +0300

    Update:

--- js/my-office.js
+++ js/my-office.js
@@ -17,48 +17,49 @@ var createBlock = (index, color, author, origin, current) => {
   return { index, color, author, origin, current };
 };
 
+var splitText = text => {
+  let textSplitted = text.split("\n");
+  return textSplitted.map((element, index) => {
+    return createBlock(index, "white", "any", element, element);
+  });
+};
+
+var mockDocument = `Мой 1 абзац. С точной и без
+
+Мой 2 изменённый абзац
+
+Мой 3 удалённый абзац
+
+Мой 4 добавленный абзац
+
+Мой 5 перемещенный абзац`;
+
+var mockOtherDocument1 = `Измен. Мой 1 абзац. С точной и без
+
+Мой 2 изменённый абзац
+
+Мой 3 удалённый абзац
+
+Мой 4 добавленный абзац
+
+Мой 5 перемещенный абзац`;
+
+var mockOtherDocument2 = `Мой 1 абзац. С точной и без
+
+Мой 2 изменённый абзац
+
+Мой 3 удалённый абзац
+
+Мой 4 добавленный абзац
+
+Мой 5 перемещенный абзац`;
+
 var app = new Vue({
   el: "#app",
   data: {
     modeSelected: "Абзац",
     paragraph: 0,
-    document: [
-      createBlock(
-        0,
-        "white",
-        "misha",
-        "Мой 1 абзац. С точной и без",
-        "Мой 1 абзац. С точной и без"
-      ),
-      createBlock(
-        1,
-        "green",
-        "misha",
-        "Мой 2 изменённый абзац",
-        "Мой 2 изменённый абзац"
-      ),
-      createBlock(
-        2,
-        "gray",
-        "misha",
-        "Мой 3 удалённый абзац",
-        "Мой 3 удалённый абзац"
-      ),
-      createBlock(
-        3,
-        "blue",
-        "misha",
-        "Мой 4 добавленный абзац",
-        "Мой 4 добавленный абзац"
-      ),
-      createBlock(
-        4,
-        "yellow",
-        "misha",
-        "Мой 5 перемещенный абзац",
-        "Мой 5 перемещенный абзац"
-      )
-    ],
+    texts: [mockDocument, mockOtherDocument1, mockOtherDocument2],
 
     otherDocuments: [
       [
@@ -174,42 +175,14 @@ var app = new Vue({
       ]
     ]
   },
-  computed: {}
+  computed: {
+    document() {
+      return splitText(this.texts[0]);
+    },
+    otherDocuments() {
+      array.forEach(element => {
+        
+      });
+    }
+  }
 });
-
-var splitText = text => {
-  let textSplitted = text.split("\n");
-  return textSplitted.map((element, index) => {
-    return  createBlock(index, "white", "any", element, element);
-  });
-};
-
-var mockDocument = `Мой 1 абзац. С точной и без
-
-Мой 2 изменённый абзац
-
-Мой 3 удалённый абзац
-
-Мой 4 добавленный абзац
-
-Мой 5 перемещенный абзац`;
-
-var mockOtherDocument1 = `Мой 1 абзац. С точной и без
-
-Мой 2 изменённый абзац
-
-Мой 3 удалённый абзац
-
-Мой 4 добавленный абзац
-
-Мой 5 перемещенный абзац`;
-
-var mockOtherDocument2 = `Мой 1 абзац. С точной и без
-
-Мой 2 изменённый абзац
-
-Мой 3 удалённый абзац
-
-Мой 4 добавленный абзац
-
-Мой 5 перемещенный абзац`;
