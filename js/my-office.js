c08b50022fc849e647ed5bd4ca204c7cdf1ae860
Lyumih <32732179+Lyumih@users.noreply.github.com>
2019-09-08 06:42:02 +0300

    Update:fix reactive

--- js/my-office.js
+++ js/my-office.js
@@ -1,11 +1,11 @@
 Vue.component("block-change", {
-  props: ["block", "color"],
+  props: ["paragraph", "text", "color"],
   template: `<div class="card shadow-sm mb-3">
               <div class="card-body text-center" :style="color==='lightcoral'? 'background-color: rgba(255, 0, 0, 0.07);': ''">
                 <div><button class="btn btn-sm btn-block">...</button></div>
-                <div :style="{background:color}">{{block.current}}</div>
+                <div :style="{background:color}">{{text}}</div>
                 <div><button class="btn btn-sm btn-block">...</button></div>
-                <a class="btn btn-sm btn-outline-success" title="Согласен"><i class="fa fa-check" aria-hidden="true"></i></a>
+                <a class="btn btn-sm btn-outline-success" title="Согласен" @click="$emit('done-changes', paragraph, text)"><i class="fa fa-check" aria-hidden="true"></i></a>
                 <a class="btn btn-sm btn-outline-danger" title="Не согласен"><i class="fa fa-times" aria-hidden="true"></i></a>
                 <a class="btn btn-sm btn-outline-primary" title="Внести правку"><i class="fa fa-pencil" aria-hidden="true"></i></a>
                 <a class="btn btn-sm btn btn-outline-warning" title="Оставить оригинал"><i class="fa fa-clock-o" aria-hidden="true"></i></a>
@@ -14,14 +14,14 @@ Vue.component("block-change", {
               `
 });
 
-var createBlock = (index, color, author, origin, current) => {
-  return { index, color, author, origin, current };
+var createBlock = (index, text) => {
+  return { index, text };
 };
 
 var splitText = text => {
-  let textSplitted = text.split("\n");
-  return textSplitted.map((element, index) => {
-    return createBlock(index, "white", "any", element, element);
+  let textSplitted = text;
+  return textSplitted.map((text, index) => {
+    return createBlock(index, text);
   });
 };
 
@@ -61,61 +61,60 @@ var app = new Vue({
     modeSelected: "Абзац",
     paragraph: 0,
     change: 0,
-    texts: [mockDocument, mockOtherDocument1, mockOtherDocument2]
+    texts: []
+  },
+  created() {
+    this.texts = [
+      mockDocument.split("\n"),
+      mockOtherDocument1.split("\n"),
+      mockOtherDocument2.split("\n")
+    ];
   },
   computed: {
-    document() {
-      return splitText(this.texts[0]);
-    },
-    otherDocuments() {
-      let others = [];
-      for (let i = 1; i < this.texts.length; i++) {
-        others.push(splitText(this.texts[i]));
-      }
-      return others;
-    },
-    documentHash() {
-      hashs = [];
-      this.document.forEach(element => {
-        hashs.push(sha256(element.current));
-      });
-      return hashs;
-    },
-    otherDocumentsHash() {
-      let otherHash = [];
-      this.otherDocuments.forEach(element => {
-        let document = [];
-        element.forEach(subelement => {
-          document.push(sha256(subelement.current));
+    documentsHash() {
+      return this.texts.map(element => {
+        return element.map(subelement => {
+          return this.hashing(subelement);
         });
-        otherHash.push(document);
       });
-      return otherHash;
     },
-    documentColor() {
-      let colors = [];
-      for (let i = 0; i < this.documentHash.length; i++) {
-        let hash = this.documentHash[i];
+    documentsColor() {
+      colors = [];
+      colors[0] = [];
+      let original = this.documentsHash[0];
+
+      original.forEach((item, index) => {
         let color = "lightgreen";
-        this.otherDocumentsHash.forEach(otherDocument => {
-          if (otherDocument[i] !== hash) {
-            color = "lightcoral";
+        this.documentsHash.slice(1).forEach(element => {
+          if (element[index] !== item) color = "lightcoral";
+        });
+
+        colors[0].push(color);
+      });
+
+      this.documentsHash.slice(1).forEach((item, indexArray) => {
+        colors[indexArray + 1] = [];
+        item.forEach((hash, index) => {
+          if (hash === this.documentsHash[0][index])
+            colors[indexArray + 1].push("lightgreen");
+          else {
+            colors[indexArray + 1].push("lightcoral");
           }
         });
-        colors.push(color);
-      }
-      return colors;
-    },
-    otherDocumentsColor() {
-      let otherColors = [];
-      this.otherDocumentsHash.forEach(otherHashs => {
-        if (otherHashs[this.paragraph] === this.documentHash[this.paragraph]) {
-          otherColors.push("lightgreen");
-        } else {
-          otherColors.push("lightcoral");
-        }
       });
-      return otherColors;
+
+      return colors;
+    }
+  },
+  methods: {
+    doneChanges(paragraph, newText) {
+      for (let i = 0; i < this.texts.length; i++) {
+        this.texts[i][paragraph] = newText;
+      }
+      this.texts.push(this.texts.pop());
+    },
+    hashing(text) {
+      return md5(text);
     }
   }
 });
